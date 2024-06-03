6a55456712fc9ef626d0d24a2fa7608fe5e0b952
adityarahman47 <aditya.ay802@gmail.com>
2024-06-03 09:20:06 +0800

    Project UAS

---
+++ lib/splash_screen/splashScreen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_flutter/page_home/home_page.dart';
+
+class HomeSplash extends StatelessWidget {
+  const HomeSplash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  "Kelompok17.News",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20,),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()));
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                ),
+                child: Text(
+                  "Lihat Berita Terkini",
+                  style: GoogleFonts.poppins(
+                    color: Colors.black
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
